d7c999e2f3e26d133f012ec5cbff5510b5bf13fa
bridgebooktakuya <t.hashimo.s@gmail.com>
2018-12-04 14:17:26 +0900

    save till bas16

---
+++ bas16.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct person {
+  char name[30];
+  char email[40];
+} personData;
+
+typedef struct company {
+  personData ceo;
+  personData employees[100];
+} companyData;
+
+int main(){
+ companyData c;
+ scanf("%s %s", c.ceo.name, c.ceo.email);
+ int i = 0;
+ while( scanf("%s %s", c.employees[i].name, c.employees[i].email) != EOF){
+  i++;
+};
+
+ printf("CEOの名前: %s\n",  c.ceo.name);
+ printf("CEOのアドレス: %s\n", c.ceo.email);
+ printf("\n");
+
+ for(i = 0; i < 5; i++){
+   printf("名前: %s\n",  c.employees[i].name);
+   printf("アドレス: %s\n", c.employees[i].email);
+   printf("\n");
+ };
+}
